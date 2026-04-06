33d7d7c8e5e6e5fa5571facb81cd8e84be01e096
ngochanmc2021-droid <ngochanmc2021@gmail.com>
2026-04-06 14:21:59 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const movies = [
+  {
+    title: "Titanic",
+    year: 1997,
+    genre: ["Romance", "Drama"],
+    poster: "images/titanic.jpg",
+    description: "Phim tình cảm kinh điển..."
+  },
+  {
+    title: "The Avengers",
+    year: 2012,
+    genre: ["Action", "Sci-Fi"],
+    poster: "images/avengers.jpg",
+    description: "Biệt đội siêu anh hùng..."
+  }
+];
